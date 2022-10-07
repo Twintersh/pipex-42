bb4f2001e492d03efd79aa352b8605396baaed4c
Twintersh <tom.wintersheim@gmail.com>
2022-10-07 03:01:36 +0200

    small bug fix

--- src/access.c
+++ src/access.c
@@ -6,7 +6,7 @@
 /*   By: twinters <twinters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 21:48:24 by twinters          #+#    #+#             */
-/*   Updated: 2022/10/05 12:03:09 by twinters         ###   ########.fr       */
+/*   Updated: 2022/10/07 02:59:17 by twinters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,10 @@ char	*get_cmd_path(char *cmd, char **envp)
 	int		i;
 
 	i = 0;
-	if (*cmd == '/')
-		return (NULL);
 	if (!access(cmd, F_OK))
 		return (ft_strjoin("", cmd));
+	if (*cmd == '/')
+		return (NULL);
 	path = ft_split(get_path(envp) + 5, ':');
 	cmd_p = add_path(path[i], cmd);
 	while (path[i] && access(cmd_p, F_OK))
